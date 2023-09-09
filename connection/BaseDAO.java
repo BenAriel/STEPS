9e162fc731502541250d4f646469345d201b5d44
FolkBlues7 <126624970+FolkBlues7@users.noreply.github.com>
2023-09-09 18:09:14 -0300

    Atualizando

--- connection/BaseDAO.java
+++ connection/BaseDAO.java
@@ -4,7 +4,7 @@ public interface BaseDAO<E> {
 	public void buscar(E objeto);
 	public void alterar(E objeto);
 	public void remover(E objeto);
-	public void listar(E objeto);
+	public void listar();
 	public void inserir(E objeto);
 }
 
