92827a538f882b248292b8767bf0766f7f858730
FolkBlues7 <126624970+FolkBlues7@users.noreply.github.com>
2023-09-19 11:36:45 -0300

    Update Prova.java

--- entities/Prova.java
+++ entities/Prova.java
@@ -1,69 +1,97 @@
 package entities;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+import java.sql.Date;
 
 public class Prova {
-	private String disciplina;
-	private String questoes;
-	private int numeroDeQuestoes;
-	private String data;
-	private int questoesFaceis;
-	private int questoesMedias;
-	private int questoesDificeis;
     private int codigo;
-	
-    public Prova(String disciplina,int codigo, String questoes, int numeroDeQuestoes, int questoesFaceis, int questoesMedias, int questoesDificeis,String data) {
+    private String disciplina;
+    private List<Questao> questaoList = new ArrayList<>();
+    private int numeroDeQuestoes;
+    private LocalDate data;
+    private int questoesFaceis;
+    private int questoesMedias;
+    private int questoesDificeis;
+
+    
+    public Prova(String disciplina, List<Questao> questaoList, int numeroDeQuestoes, int questoesFaceis, int questoesMedias, int questoesDificeis) {
         setDisciplina(disciplina);
-        setCodigo(codigo);
-        setQuestoes(questoes);
+        setQuestoes(questaoList);
         setNumeroDeQuestoes(numeroDeQuestoes);
         setQuestoesFaceis(questoesFaceis);
         setQuestoesMedias(questoesMedias);
         setQuestoesDificeis(questoesDificeis);
-        setData(data);
+        setData(LocalDate.now()); 
     }
-		
-	public void gerarProvas(int numeroDeQuestoes,int questoesFaceis, int questoesMedias, int questoesDificeis,String data) {
-	    setNumeroDeQuestoes(numeroDeQuestoes);
-		setQuestoesFaceis(questoesFaceis);
-	    setQuestoesMedias(questoesMedias);
-	    setQuestoesDificeis(questoesDificeis);  
-	    setData(data); // coloca a data do momento da criação como data da prova
-	}
-	
-    public String getDisciplina() {
+    
+    //construtor sem lista de questoes
+    public Prova(String disciplina, int numeroDeQuestoes, int questoesFaceis, int questoesMedias, int questoesDificeis) {
+        setDisciplina(disciplina);
+        setNumeroDeQuestoes(numeroDeQuestoes);
+        setQuestoesFaceis(questoesFaceis);
+        setQuestoesMedias(questoesMedias);
+        setQuestoesDificeis(questoesDificeis);
+        setData(LocalDate.now()); 
+    }
+
+    //construtor para pesquisa
+    public Prova(int codigo) {
+        this.codigo = codigo;
+    }
+    
+   //construtor para alterar
+    public Prova(int codigo,String disciplina, int numeroDeQuestoes, int questoesFaceis, int questoesMedias, int questoesDificeis) {
+        setCodigo(codigo);
+    	setDisciplina(disciplina);
+        setQuestoes(questaoList);
+        setNumeroDeQuestoes(numeroDeQuestoes);
+        setQuestoesFaceis(questoesFaceis);
+        setQuestoesMedias(questoesMedias);
+        setQuestoesDificeis(questoesDificeis);
+        setData(LocalDate.now()); 
+    }
+    
+    //construtor padrão
+    public Prova() {
     	
+    }
+
+    public void gerarProvas(int numeroDeQuestoes, int questoesFaceis, int questoesMedias, int questoesDificeis) {
+    	setCodigo(1); //inicia sempre com um para evitar erros na classe DAO!
+        setNumeroDeQuestoes(numeroDeQuestoes);
+        setQuestoesFaceis(questoesFaceis);
+        setQuestoesMedias(questoesMedias);
+        setQuestoesDificeis(questoesDificeis);  
+        setData(LocalDate.now()); // coloca a data do momento da criação como data da prova
+    }
+    
+    private void setData(LocalDate now) {
+        this.data = LocalDate.now();
+    }
+
+    public List<Questao> getQuestaoList() {
+        return questaoList;
+    }
+
+    public void setQuestoes(List<Questao> questaoList) {
+        if (questaoList != null) {
+            this.questaoList = questaoList;
+        } else {
+            System.out.println("Valor inválido para a lista de questões");
+        }
+    }
+
+    public String getDisciplina() {
         return disciplina;
     }
 
     public void setDisciplina(String disciplina) {
-    	if(disciplina != null) {
-			this.disciplina = disciplina;
-		}
-		else
-		{
-			System.out.println("Disciplina não pode ser vazia!");
-		}
-    }
-
-    public String getQuestoes() {
-        return questoes;
-    }
-
-    public void setQuestoes(String questoes) {
-    	if(questoes != null) {
-			this.questoes = questoes;
-		}
-		else
-		{
-			System.out.println("Questoes não pode ser vazia!");
-		}
-    }
-    public int getCodigo() {
-        return codigo;
-    }
-    public void setCodigo(int codigo) {
-        if(codigo!=0)
-        this.codigo = codigo;
+        if (disciplina != null) {
+            this.disciplina = disciplina;
+        } else {
+            System.out.println("Disciplina não pode ser vazia!");
+        }
     }
 
     public int getNumeroDeQuestoes() {
@@ -71,26 +99,24 @@ public class Prova {
     }
 
     public void setNumeroDeQuestoes(int numeroDeQuestoes) {
-    	if(numeroDeQuestoes < 1 ) {
-			System.out.println("Numero de questoes não pode ser negativo ou zero!");
-			System.exit(1);
-		}
-		else
-		{
-			this.numeroDeQuestoes = numeroDeQuestoes;
-		}
-    }   public String getData() {
-        return data;
+        if (numeroDeQuestoes < 1) {
+            System.out.println("Numero de questoes não pode ser negativo ou zero!");
+            System.exit(1);
+        } else {
+            this.numeroDeQuestoes = numeroDeQuestoes;
+        }
     }
 
-    public void setData(String data) {
-    	if(data != null ) {
-			this.data = data;
-		}
-		else
-		{
-			System.out.println("A data não pode ser vazio");
-		}
+    public void setData(Date localDate) {
+        if (localDate != null) {
+            this.data = localDate.toLocalDate();
+        } else {
+            System.out.println("A data não pode ser vazia");
+        }
+    }
+
+    public LocalDate getData() {
+        return this.data;
     }
 
     public int getQuestoesFaceis() {
@@ -150,4 +176,26 @@ public class Prova {
             this.questoesDificeis = questoesDificeis;
         }
     }
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		if (codigo > 0) {
+		this.codigo = codigo;
+		} else {
+			System.out.println("O código precisa ser maior quer 0!");
+		}
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Prova [codigo=" + codigo + ", disciplina=" + disciplina + ", questaoList=" + questaoList
+				+ ", numeroDeQuestoes=" + numeroDeQuestoes + ", data=" + data + ", questoesFaceis=" + questoesFaceis
+				+ ", questoesMedias=" + questoesMedias + ", questoesDificeis=" + questoesDificeis + "]";
+	}
+
+	
 }
