b6dd6bfe31a829ca38d81d6aad0630baac47ba97
FolkBlues7 <126624970+FolkBlues7@users.noreply.github.com>
2023-09-26 20:49:07 -0300

    Completando o Controller de cadastro

--- Sistema/src/controller/ControllerCadastro.java
+++ Sistema/src/controller/ControllerCadastro.java
@@ -1,37 +1,39 @@
 package controller;
 
-import javafx.event.ActionEvent;
+import connection.MonitorDAO;
+import entities.Monitor;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class ControllerCadastro {
-
-    @FXML
-    private Button finalizarcadastro;
-
-    @FXML
-    private TextField loginprofessor;
-
-    @FXML
-    private TextField nomecompleto;
-
-    @FXML
-    private PasswordField novasenha;
-
-    @FXML
-    private TextField novologin;
-
-    @FXML
-    private PasswordField repetirsenha;
-
-    @FXML
-    private PasswordField senhaprofessor;
-
-    @FXML
-    void finalizarcadastro(ActionEvent event) {
-
-    }
-
+	@FXML private TextField nome;
+	@FXML private TextField novologin;
+	@FXML private TextField novasenha;
+	@FXML private TextField matricula;
+	@FXML private TextField repetirsenha;
+	@FXML private TextField loginprofessor;
+	@FXML private TextField senhaprofessor;
+	@FXML private Label error;
+	@FXML private Button btCadastrar;
+	
+	public void cadastrar() {
+		Monitor monitor = new Monitor();
+		MonitorDAO buscador = new MonitorDAO();
+		monitor.setLogin(novologin.getText());
+		monitor.setSenha(novasenha.getText());
+		monitor.setNomeMonitor(nome.getText());
+		monitor.setMatricula(matricula.getText());
+		String textoRepitirSenha = repetirsenha.getText();
+		String loginProfessor = loginprofessor.getText();
+		String senhaProfessor = senhaprofessor.getText();
+		if (textoRepitirSenha.equals(monitor.getSenha()) && loginProfessor.equals("admin123") && senhaProfessor.equals("administrador123")) {
+		    System.out.println("As senhas são iguais!");
+		    buscador.inserir(monitor);
+		} else {
+		    System.out.println("As senhas não são iguais!");
+		    error.setVisible(true);
+		}
+	}
 }
